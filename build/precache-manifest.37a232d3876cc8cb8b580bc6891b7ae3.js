f4e8e3abb94a6800ce5a4b59a145bc212f28a27d
Renan de Carvalho Ribeiro <renancr176@gmail.com>
2020-03-25 10:58:12 -0300

    Criado arquivos para o github pages

---
+++ build/precache-manifest.37a232d3876cc8cb8b580bc6891b7ae3.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "638b1c03aaa693913173366a2ccc7cc5",
+    "url": "/ReactMonstersRolodex/index.html"
+  },
+  {
+    "revision": "027d2933754fd1b2131f",
+    "url": "/ReactMonstersRolodex/static/css/main.ddff0aa7.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/ReactMonstersRolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/ReactMonstersRolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "027d2933754fd1b2131f",
+    "url": "/ReactMonstersRolodex/static/js/main.f7970cd4.chunk.js"
+  },
+  {
+    "revision": "7561d6095f7cb29aef5b",
+    "url": "/ReactMonstersRolodex/static/js/runtime-main.ac70f9cf.js"
+  },
+  {
+    "revision": "ee7cd8ed2dcec943251eb2763684fc6f",
+    "url": "/ReactMonstersRolodex/static/media/logo.ee7cd8ed.svg"
+  }
+]);
